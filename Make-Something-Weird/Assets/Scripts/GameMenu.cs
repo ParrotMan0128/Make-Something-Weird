9c7b4afe3945f0cb4e4151424771f529c0d8a30b
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-04-08 20:23:12 +0900

    몬가.. 메인 메뉴가.. 추가됢..

--- Make-Something-Weird/Assets/Scripts/GameMenu.cs
+++ Make-Something-Weird/Assets/Scripts/GameMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4f21f464e5ec05694c450f536805868b4e174b627fee289325031425911cc30
-size 1406
+oid sha256:a78736ee641a0b0e7d78ed8b43ddf236b48fddef6e5393db7bdcb4d902100c69
+size 1546
